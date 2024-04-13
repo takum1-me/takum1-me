75aa88c76a1bbdbd12c5ca3fedf7e77bdbb28693
もにょっとしたぷにぷにのなにか <134484570+monyopuni@users.noreply.github.com>
2024-04-13 20:18:07 +0900

    文を追加。

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
 export default function Home() {
   return (
     <main>
+
+      <h1>ひっそりと、大胆に。</h1>
+      <p>自分がつくりたかったものを、こっそりと貯めていく場所。</p>
+
       <div>accounts</div>
       <ul>
         <li>twitter</li>
