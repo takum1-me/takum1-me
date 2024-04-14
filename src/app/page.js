4b2b64ac21d32f29037b7d0b436c91dce616cc48
もにょっとしたぷにぷにのなにか <134484570+monyopuni@users.noreply.github.com>
2024-04-14 16:39:46 +0900

    imageモジュールをimport,トップページ画像を追加・整理

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,16 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <main>
 
       <h1>ひっそりと、大胆に。</h1>
       <p>自分がつくりたかったものを、こっそりと貯めていく場所。</p>
+      <Image
+        src="/daidai-toppage.JPG"
+        alt="トップページのだいだいの画像"
+        width={1500}
+        height={1000} />
 
       <div>accounts</div>
       <ul>
