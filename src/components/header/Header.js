ca809ea310add58ea2753e9494fdeaba9dd4df53
もにょっとしたぷにぷにのなにか <134484570+monyopuni@users.noreply.github.com>
2024-04-15 21:00:14 +0900

    headerの中身を記載

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,35 @@
+import Link from 'next/link';
+import '../../styles/global.css';
+
 const Header = () => {
     return (
         <header>
+            <h1>takum1.me</h1>
+
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/">
+                            <a>Home</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about">
+                            <a>Profile</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/projects">
+                            <a>Blog</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">
+                            <a>Works</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 };
