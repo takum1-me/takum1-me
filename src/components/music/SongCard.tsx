961fb6bf563c59bb142162eef98e5587c59c30cc
takum1 <takum1.k@coins.tsukuba.ac.jp>
2025-09-21 09:39:27 +0900

    aaa

--- src/components/music/SongCard.tsx
+++ src/components/music/SongCard.tsx
@@ -5,12 +5,17 @@ interface SongCardProps {
   artworkUrl: string;
 }
 
-export default function SongCard({ title, artist, appleMusicUrl, artworkUrl }: SongCardProps) {
+export default function SongCard({
+  title,
+  artist,
+  appleMusicUrl,
+  artworkUrl,
+}: SongCardProps) {
   return (
-    <a 
-      href={appleMusicUrl} 
-      target="_blank" 
-      rel="noopener noreferrer" 
+    <a
+      href={appleMusicUrl}
+      target="_blank"
+      rel="noopener noreferrer"
       className="song-card"
     >
       <img src={artworkUrl} alt={`${title} - ${artist}`} />
